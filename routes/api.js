b1052ce50070316d7d334c1a6c4eb77fd7da058b
darthNater <blstarwars@live.com>
2021-01-21 22:23:27 -0700

    got the seed running

---
+++ routes/api.js
@@ -0,0 +1,46 @@
+const router = require("express").Router();
+const Workout = require("../models/workout.js");
+
+router.post("/api/workouts", ({ body }, res) => {
+  Workout.create(body)
+    .then(dbTransaction => {
+      res.json(dbTransaction);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+router.put("/api/workouts/:id", ({ body, params : {id} }, res) => {
+  Workout.findOneAndUpdate({_id: id}, { $push: { exercises: body } }, { new: true })
+    .then(dbTransaction => {
+      res.json(dbTransaction);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+router.get("/api/workouts", (req, res) => {
+  Workout.find({})
+    .sort({ date: -1 })
+    .then(dbTransaction => {
+      res.json(dbTransaction);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+router.get("/api/workouts/range", (req, res) => {
+  Workout.find({})
+    .sort({ date: -1 })
+    .then(dbTransaction => {
+      res.json(dbTransaction);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+module.exports = router;
